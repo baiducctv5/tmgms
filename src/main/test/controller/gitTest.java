496dc545b417b4f664a250faa8585501089d6491
guogang <guogang@staff.chinabyte.com>
2017-03-06 21:16:04 +0800

    修改了测试类，为学习debug提供环境

--- src/main/test/controller/gitTest.java
+++ src/main/test/controller/gitTest.java
@@ -1,11 +1,21 @@
 package controller;
 
+import org.junit.Test;
+
 /**
  * Created by guogang on 2017/3/6.
  */
 public class gitTest {
-    public static void main(String[] args) {
-        System.out.println("i m gitTest");
+    public void getMsg(){
+        System.out.println("执行方法开始");
+        forMsg();
+        System.out.println("执行方法结束");
+    }
+
+    public void forMsg(){
+        for(int i=0;i<10;i++){
+            System.out.println("循环的次数："+i);
+        }
     }
 
 }
