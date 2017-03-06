028e0ee93dd5f4bfbe675c49f9a97635d689c9bd
guogang <guogang@staff.chinabyte.com>
2017-03-06 18:01:35 +0800

    创建测试test方法

---
+++ src/main/test/controller/gitTest.java
@@ -0,0 +1,11 @@
+package controller;
+
+/**
+ * Created by guogang on 2017/3/6.
+ */
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("i m gitTest");
+    }
+
+}
